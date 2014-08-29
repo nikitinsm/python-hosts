58e98e775916053be2499dfe90eced66742c5e95
nikitinsm <nikitinsm@gmail.com>
2014-08-29 10:55:34 +0400

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup\
+    ( name='Python-hosts-util'
+    , version='1.0'
+    , description='Python /etc/hosts utility'
+    , author='Sergey Nikitin'
+    , author_email='nikitinsm@gmail.com'
+    , url='https://github.com/nikitinsm/python-hosts'
+    , packages=['python_hosts']
+    )
--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,19 @@ class BaseTest(unittest.TestCase):
 
     def test_parse_1(self):
         data = parse_file(os.path.normpath(os.path.join(os.path.dirname(__file__), 'data', 'hosts-simple')))
+        self.assertEqual\
+            ( data
+            , [ '# wrong config'
+              , ('127.0.0.1', {'localhost', })
+              , '# proper config'
+              , ('127.0.0.1', {'node1.localhost', 'node.local', 'localhost'})
+            ] )
 
     def test_build_1(self):
         data = parse_file(os.path.normpath(os.path.join(os.path.dirname(__file__), 'data', 'hosts-simple')))
-        print build_file(data)
+        content = build_file(data)
+        compare = """# wrong config
+127.0.0.1	localhost
+# proper config
+127.0.0.1	node1.localhost node.local localhost"""
+        self.assertEqual(content, compare)
